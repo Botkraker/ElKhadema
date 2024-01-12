2da0eddc791e6b59b6a550842f8e62416bee41d6
jbuguy <wassimnefzi120@gmail.com>
2024-01-13 00:09:31 +0100

    added connection

--- khadema/src/main/java/module-info.java
+++ khadema/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Elkhadema.khadema {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens Elkhadema.khadema to javafx.fxml;
     exports Elkhadema.khadema;

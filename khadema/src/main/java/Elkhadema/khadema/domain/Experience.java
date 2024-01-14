0d72a8c7e2d6cd1b48642758924fc4e1c4ac9933
jbuguy <wassimnefzi120@gmail.com>
2024-01-14 13:16:37 +0100

    added domain model

---
+++ khadema/src/main/java/Elkhadema/khadema/domain/Experience.java
@@ -0,0 +1,49 @@
+package Elkhadema.khadema.domain;
+
+public class Experience {
+    private String description;
+    private String mission;
+    private String type;
+    private String technologie;
+
+    public Experience(String description, String mission, String type, String technologie) {
+        this.description = description;
+        this.mission = mission;
+        this.type = type;
+        this.technologie = technologie;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+
+    public void setMission(String mission) {
+        this.mission = mission;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTechnologie() {
+        return technologie;
+    }
+
+    public void setTechnologie(String technologie) {
+        this.technologie = technologie;
+    }
+
+    
+}
--- khadema/src/main/java/module-info.java
+++ khadema/src/main/java/module-info.java
@@ -4,5 +4,7 @@ module Elkhadema.khadema {
     requires java.sql;
 
     opens Elkhadema.khadema to javafx.fxml;
+    opens Elkhadema.khadema.controller to javafx.fxml;
     exports Elkhadema.khadema;
+    exports Elkhadema.khadema.controller;
 }

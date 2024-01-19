ec84cdc355672eb64e6da68416424c0ac832586f
yassine <yassinebensassi321@gmail.com>
2024-01-19 09:08:08 +0100

    fixing admin

--- src/main/java/Elkhadema/khadema/domain/Admin.java
+++ src/main/java/Elkhadema/khadema/domain/Admin.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Admin extends User{
 
-	public Admin(` id, String password, ContactInfo contactInfo, String firstname, String lastname, Date creationDate,
+	public Admin(int id, String password, ContactInfo contactInfo, String firstname, String lastname, Date creationDate,
 			Date lastloginDate, boolean is_banned, boolean is_active) {
 		super(id, password, contactInfo, firstname, lastname, creationDate, lastloginDate, is_banned, is_active);
 		// TODO Auto-generated constructor stub

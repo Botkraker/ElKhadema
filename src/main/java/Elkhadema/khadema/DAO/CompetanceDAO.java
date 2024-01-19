8224951d6586a5777a70498026026b0f0bd6822e
yassine <yassinebensassi321@gmail.com>
2024-01-19 09:11:23 +0100

    Creating CompetanceDAO

---
+++ src/main/java/Elkhadema/khadema/DAO/CompetanceDAO.java
@@ -0,0 +1,40 @@
+package Elkhadema.khadema.DAO;
+
+import java.util.List;
+import java.util.Optional;
+
+import Elkhadema.khadema.domain.Company;
+
+public class CompetanceDAO implements CompanyDAOINT {
+
+	@Override
+	public Optional<Company> get(long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<Company> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Company t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Company t, Company newT) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Company t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/Elkhadema/khadema/DAO/CompetanceDAOINT.java
@@ -0,0 +1,7 @@
+package Elkhadema.khadema.DAO;
+
+import Elkhadema.khadema.domain.Competance;
+
+public interface CompetanceDAOINT extends Dao<Competance>{
+	
+}

72fb079c96b9d6362e4aaec116aaf2d149db523b
jbuguy <wassimnefzi120@gmail.com>
2024-03-19 11:09:15 +0100

    added date to job request to help in notification

--- src/main/java/Elkhadema/khadema/domain/JobRequest.java
+++ src/main/java/Elkhadema/khadema/domain/JobRequest.java
@@ -1,14 +1,25 @@
 package Elkhadema.khadema.domain;
 
+import java.util.Date;
+
 public class JobRequest {
     private User user;
     private JobOffre jobOffre;
+    private Date date;
     private String etat;
 
     public JobRequest(User user, JobOffre jobOffre, String etat) {
         this.user = user;
         this.jobOffre = jobOffre;
         this.etat = etat;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public User getUser() {

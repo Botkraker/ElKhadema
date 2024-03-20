af9ca3c9d33c1d679363e31c696d0c9d55b49884
jbuguy <wassimnefzi120@gmail.com>
2024-03-20 17:28:21 +0100

    commit

---
+++ src/main/java/Elkhadema/khadema/controller/MainPageController.java
@@ -0,0 +1,16 @@
+package Elkhadema.khadema.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainPageController implements Initializable{
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+}

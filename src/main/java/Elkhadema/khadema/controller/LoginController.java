43d02ee27e4d02ff549c017c7f539a056895f60e
jbuguy <wassimnefzi120@gmail.com>
2024-03-21 15:11:53 +0100

    login change

--- src/main/java/Elkhadema/khadema/controller/LoginController.java
+++ src/main/java/Elkhadema/khadema/controller/LoginController.java
@@ -6,11 +6,20 @@ import Elkhadema.khadema.Service.ServiceImplemantation.UserServiceImp;
 import Elkhadema.khadema.Service.ServiceInterfaces.UserService;
 import Elkhadema.khadema.domain.ContactInfo;
 import Elkhadema.khadema.domain.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController {
+    private Scene Scene;
+    private Stage stage;
+    private Parent root;
     // new user to add
     User user = new User(0, null, null);
     ContactInfo contactInfo = new ContactInfo(0);
@@ -24,7 +33,7 @@ public class LoginController {
     @FXML
     Text invalid;
     @FXML
-    public void login() throws IOException{
+    public void login(ActionEvent event) throws IOException{
         try {
             user= userService.Login(name.getText(), password.getText());
 
@@ -34,9 +43,15 @@ public class LoginController {
         }
         if (user==null) {
             invalid.setText("password invalid");
+            return;
         }
-
-        App.setRoot("mainpage");
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Elkhadema/khadema/mainpage.fxml"));
+        MainPageController mainPageController=loader.getController();
+        root =loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML

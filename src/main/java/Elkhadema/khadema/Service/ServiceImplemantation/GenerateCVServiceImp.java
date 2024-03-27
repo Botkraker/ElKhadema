27fd07bfb5e5b214f35b09326b6b638e86bcdf01
jbuguy <wassimnefzi120@gmail.com>
2024-03-27 14:59:57 +0100

    fixed generate cv

--- src/main/java/Elkhadema/khadema/Service/ServiceImplemantation/GenerateCVServiceImp.java
+++ src/main/java/Elkhadema/khadema/Service/ServiceImplemantation/GenerateCVServiceImp.java
@@ -3,6 +3,7 @@ package Elkhadema.khadema.Service.ServiceImplemantation;
 import java.io.IOException;
 
 import Elkhadema.khadema.DAO.DAOImplemantation.CompetanceDAO;
+import Elkhadema.khadema.DAO.DAOImplemantation.ContactInfoDAO;
 import Elkhadema.khadema.DAO.DAOImplemantation.ExperienceDAO;
 import Elkhadema.khadema.Service.ServiceInterfaces.GenerateCVService;
 import Elkhadema.khadema.domain.Person;
@@ -11,8 +12,10 @@ import Elkhadema.khadema.util.CVgenerator;
 public class GenerateCVServiceImp implements GenerateCVService{
     private ExperienceDAO experienceDAO=new ExperienceDAO();
     private CompetanceDAO competanceDAO=new CompetanceDAO();
+    private ContactInfoDAO contactInfoDAO =new ContactInfoDAO();
     @Override
     public void generateCV(Person person) throws IOException {
+        person.setContactInfo(contactInfoDAO.get(person.getContactInfo().getId()).get());
         CVgenerator.Generate(person, "CV.pdf", competanceDAO.getAll(person), experienceDAO.getAll(person)   );
     }
 

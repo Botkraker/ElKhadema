886ee483d8f4fb6b44e135397140fa02f14837e8
jbuguy <wassimnefzi120@gmail.com>
2024-03-30 14:14:30 +0100

    changed the generate service

--- src/main/java/Elkhadema/khadema/Service/ServiceImplemantation/GenerateCVServiceImp.java
+++ src/main/java/Elkhadema/khadema/Service/ServiceImplemantation/GenerateCVServiceImp.java
@@ -9,14 +9,17 @@ import Elkhadema.khadema.Service.ServiceInterfaces.GenerateCVService;
 import Elkhadema.khadema.domain.Person;
 import Elkhadema.khadema.util.CVgenerator;
 
-public class GenerateCVServiceImp implements GenerateCVService{
-    private ExperienceDAO experienceDAO=new ExperienceDAO();
-    private CompetanceDAO competanceDAO=new CompetanceDAO();
-    private ContactInfoDAO contactInfoDAO =new ContactInfoDAO();
+public class GenerateCVServiceImp implements GenerateCVService {
+    private ExperienceDAO experienceDAO = new ExperienceDAO();
+    private CompetanceDAO competanceDAO = new CompetanceDAO();
+    private ContactInfoDAO contactInfoDAO = new ContactInfoDAO();
+
     @Override
     public void generateCV(Person person) throws IOException {
         person.setContactInfo(contactInfoDAO.get(person.getContactInfo().getId()).get());
-        CVgenerator.Generate(person, "CV.pdf", competanceDAO.getAll(person), experienceDAO.getAll(person)   );
+        // CVgenerator.Generate(person, "CV.pdf", competanceDAO.getAll(person),
+        // experienceDAO.getAll(person) );
+        CVgenerator.GenerateCvFromHTML(person, "CV.pdf", competanceDAO.getAll(person), experienceDAO.getAll(person));
     }
 
 }
